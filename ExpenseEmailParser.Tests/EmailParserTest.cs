935da62aef62887c1ba261f6cdf3601907dc48c7
Joseph <jdr81@DESKTOP-0EDHS3O>
2020-11-08 11:26:38 +1300

    Added test for email message with text before the xml value.

--- ExpenseEmailParser.Tests/EmailParserTest.cs
+++ ExpenseEmailParser.Tests/EmailParserTest.cs
@@ -119,6 +119,38 @@ namespace ExpenseEmailParser.Tests
             Assert.AreEqual(actual.Count, 1);
         }
 
+        [Test]
+        public void TextXMLWithStringAtStart_Test()
+        {            
+
+            var input = $"Dear <SupervisorName>," +
+                        "<expense>" +
+                        "<cost_centre>DEV001</cost_centre>" +
+                        "<total>11.00</total>" +
+                        "<payment_method>personal card</payment_method>" +
+                        "</expense>";
+
+            var expected = new List<ExpenseBreakdown>()
+            {
+                new ExpenseBreakdown()
+                {
+                    XmlExtracted =  $"<expense>" +
+                        "<cost_centre>DEV001</cost_centre>" +
+                        "<total>11.00</total>" +
+                        "<payment_method>personal card</payment_method>" +
+                        "</expense>",
+                    GST = "10.00%",
+                    BeforeTotal = 10
+                }
+            };
+
+            var actual = controller.ParseEmail(input);
+
+            Assert.AreEqual(expected[0].XmlExtracted, actual[0].XmlExtracted);
+            Assert.AreEqual(expected[0].GST, actual[0].GST);
+            Assert.AreEqual(expected[0].BeforeTotal, actual[0].BeforeTotal);
+            Assert.AreEqual(actual.Count, 1);
+        }
 
         #endregion
 
